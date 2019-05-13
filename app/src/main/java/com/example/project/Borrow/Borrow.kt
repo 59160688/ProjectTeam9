3fe07c525cb1f62a466f6b40ebe008a408b6e9b5
champiez <59160688@go.buu.ac.th>
2019-05-13 23:07:04 +0700

    dialog add

--- app/src/main/java/com/example/project/Borrow/Borrow.kt
+++ app/src/main/java/com/example/project/Borrow/Borrow.kt
@@ -22,25 +22,10 @@ class Borrow : AppCompatActivity() {
         var return_date = findViewById<EditText>(R.id.editText_DateReturn)
         var comment = findViewById<EditText>(R.id.editText_Comment)
 
-        val dialog = AlertDialog.Builder(this)
-        val dialogView = layoutInflater.inflate(R.layout.activity_borrow,null)
 
-        dialog.setView(dialogView)
-        dialog.setCancelable(false)
-        dialog.setPositiveButton("validate",{ dialogInterface: DialogInterface, i: Int -> })
-        val customDialog = dialog.create()
-        customDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener({
-            if(name1.text.toString()==""){
-                customDialog.dismiss()
-            }else if(date.text.toString()==""){
-                customDialog.dismiss()
-            }else if(return_date.text.toString()==""){
-                customDialog.dismiss()
-            }else if(comment.text.toString()==""){
-                customDialog.dismiss()
-            }
-        })
-        customDialog.show()
+
+
+
 
       //  btnchooseformat = findViewById(R.id.btnchooseformat) as Button
        // btnchooseformat!!.setOnClickListener {
